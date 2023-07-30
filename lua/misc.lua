1aa36aae3b1259b6b2019c1f253e0a631dde60d5
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-07-30 21:25:56 +0800

    feat(plugin): Installed and configured no-neck-pain

--- lua/misc.lua
+++ lua/misc.lua
@@ -1,3 +1,20 @@
+local bg = vim.inspect(vim.cmd.colorscheme())
+
 return {
-  'tpope/vim-commentary',
+  "tpope/vim-commentary",
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    event = "ColorScheme",
+    opts = {
+      width = 80,
+      background = bg,
+      autocmds = {
+        enableOnVimEnter = false,
+      }
+    },
+    config = function()
+      vim.cmd("NoNeckPain")
+    end
+  }
 }
