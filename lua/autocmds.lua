d84b9c4060100d142aec035e3b50323a9bc1d29c
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-08-13 09:11:12 +0800

    feat(folds): Added autocmds for folds

---
+++ lua/autocmds.lua
@@ -0,0 +1,16 @@
+vim.cmd([[
+augroup SaveFoldsWhenWriting
+    autocmd!
+    autocmd BufWritePost * mkview | filetype detect
+augroup end
+
+augroup SaveFoldsWhenQuitting
+    autocmd!
+    autocmd QuitPre * mkview | filetype detect
+augroup end
+
+augroup LoadFoldOnStartup
+  autocmd!
+  autocmd BufWinEnter * silent! loadview | filetype detect
+augroup end
+]])
