aba11431e2e05bdd73565954bdea6e851f753f3b
Yap Jia Hong <yapjiahong-p@proton.me>
2024-02-15 07:28:27 +0800

    fix: autocmd prevents omnifunc from working properly.

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,15 +1,21 @@
+--- filetype detect in the autocmds screws up the whole thing with
+--- omnifunc in the LSP. It should be removed. For now this file is
+--- no longer sourced. Once I'm sure this is the problem, I'll source
+--- it again.
+
 local folds_augroup = vim.api.nvim_create_augroup("Folds", { clear=true })
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = folds_augroup,
-    command = "mkview | filetype detect | set foldmethod=manual"
+    command = "mkview | set foldmethod=manual"
 })
 
 vim.api.nvim_create_autocmd("QuitPre", {
     group = folds_augroup,
-    command = "mkview | filetype detect | set foldmethod=manual"
+    command = "mkview | set foldmethod=manual"
 })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
     group = folds_augroup,
-    command = "silent! loadview | filetype detect | set foldmethod=manual | normal! zM"
+    command = "silent! loadview | set foldmethod=manual | normal! zM"
 })
+
