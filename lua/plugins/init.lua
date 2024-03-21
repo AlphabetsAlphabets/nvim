18a654bb6c2cd556c203f71a9594d385c5ef0e6e
Yap Jia Hong <yapjiahong-p@proton.me>
2024-03-21 20:09:53 +0800

    feat(plug): Added oil, removed neotree.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ require('gitsigns').setup()
 require("neogit").setup()
 
 require("plugins/fzf")
+require("plugins/oil")
 require("plugins/treesitter")
 require("plugins/hydras")
 require("plugins/dashboard")
